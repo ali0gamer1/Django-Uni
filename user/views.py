c1127b64615a80135aa2bcf0ee3f40a63bfe376f
Darya Taghva <darya2000a0@gmail.com>
2023-10-31 04:44:22 -0700

    userlogin succeed

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            response_data = {'message': 'Login successful', 'username': username}
+            # return JsonResponse(response_data)
+            return Response(response_data)
+        else:
+            response_data = {'message': 'Login failed'}
+            # return JsonResponse(response_data, status=401)
+            return Response(response_data)
