1e5282c36c226ba8f4a8e79135bfcbf99803f053
Darya Taghva <darya2000a0@gmail.com>
2023-10-31 07:42:07 -0700

    updated serializers.py

--- daneshgah/serializers.py
+++ daneshgah/serializers.py
@@ -1,4 +1,24 @@
 from models import *
+class UserSerializer(serializers.Serializer):# noqa
+    class Meta:
+        model = User
+        fields = '__all__'
+class StudentSerializer(serializers.Serializer):# noqa
+    class Meta:
+        model = Student
+        fields = '__all__'
+class ProfessorSerializer(serializers.Serializer):# noqa
+    class Meta:
+        model = Professor
+        fields = '__all__'
+class ITSerializer(serializers.Serializer):# noqa
+    class Meta:
+        model = IT
+        fields = '__all__'
+class DeputyofEducationSerializer(serializers.Serializer):# noqa
+    class Meta:
+        model = DeputyofEducation
+        fields = '__all__'
 class TermSerializer(serializers.Serializer):# noqa
     class Meta:
         model = Term
