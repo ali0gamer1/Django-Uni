be62ec65cf4dbec26a1b772863f2098b76e918b2
manifairid03 <manifaridi03@gmail.com>
2023-10-31 18:19:52 +0330

    Updated Project

--- main/urls.py
+++ main/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import user
-
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('user/', include('user.urls'))
+    path('admin/', admin.site.urls)
 ]
