00189b0a76856ec9e31a640967fd19cadb6deb11
manifairid03 <manifaridi03@gmail.com>
2023-11-01 17:51:00 +0330

    improved the import statement

--- daneshgah/admin.py
+++ daneshgah/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (User, Student, Professor, IT, DeputyofEducation, Term, RevisionRequest,
-                    CourseStudent, EmergencyDrop, TermDrop, EdCert, Department, Field, SelectedCourse)
+                    CourseStudent, EmergencyDrop, TermDrop, EdCert, Department, Field, SelectedCourse,
+                    AbstractCourse, TermicCourse,)
 
 
 admin.site.register(Department)
