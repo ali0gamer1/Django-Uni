408a1e705243924c1b12c477a7020c13628c2d4a
Darya Taghva <darya2000a0@gmail.com>
2023-11-01 03:21:14 -0700

    login and logout view

--- daneshgah/urls.py
+++ daneshgah/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TermAPIView,TermListAPIView
+from .views import TermAPIView, TermListAPIView, UserAPIView, LoginAPIView
+
 urlpatterns = [
     path("term/<int:pk>/", TermAPIView.as_view()),
     path("terms/", TermListAPIView.as_view()),
-    
+    path("users/", UserAPIView.as_view()),
+    path("users/login/", LoginAPIView.as_view()),
+    path("users/logout/", LogoutAPIView.as_view())
 ]
