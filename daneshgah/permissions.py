d740c0b0355c1c5c7643e52acb91b84181c63233
Darya Taghva <darya2000a0@gmail.com>
2023-11-05 07:20:18 -0800

    finished permissions.py

---
+++ daneshgah/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+from daneshgah.models import Professor, Student, IT, DeputyofEducation
+
+
+class IsProfessorPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, Professor):
+            return True
+        return False
+
+
+class IsStudentPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, Student):
+            return True
+        return False
+
+
+class IsITPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, IT):
+            return True
+        return False
+
+
+class IsDeputyOfEducationPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isinstance(request.user, DeputyofEducation):
+            return True
+        return False
