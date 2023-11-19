407b4b2c1f2d65e149e355e22cae9d7972d5dfe8
Darya Taghva <darya2000a0@gmail.com>
2023-11-19 05:06:42 -0800

    writing test for the required views.

--- daneshgah/tests.py
+++ daneshgah/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import json
 
+from django.test import TestCase
+import rest_framework
+from rest_framework.test import APIRequestFactory
 # Create your tests here.
+
+#basic test
+# class MyTest(TestCase):
+#     def setup(self):
+#         print('testing')
+#     def tearDown(self):
+#         print('tearing down')
+#     def testme(self):
+#         print('hello')
+
+factory = APIRequestFactory()
+request = factory.post('/users/login', json.dump({'username': 'darya', 'password': '1234'}), content_type=
+'application/json')
+request1 = factory.post('/users/logout', content_type=
+'application/json')
+requestChangePassword = factory.post('/users/changePassword', json.dump({
+    'username': 'darya',
+    'password': '1234',
+    'newPassword': '2468032'
+}), content_type='application/json')
